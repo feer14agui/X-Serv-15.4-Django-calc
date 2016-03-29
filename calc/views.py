9019db5281d11dfdd6152800f8db426eebd8475a
Fernando Aguilar Santos <fsantos@zeta40.aulas.gsyc.es>
2016-03-29 09:18:46 +0200

    paquete views

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def suma (request, num1, num2):
+
+	sum = int(num1) + int(num2)
+
+	return HttpResponse ("El resultado es " + str(sum))
+
+def resta (request, num1, num2):
+
+	rest =  int(num1) - int(num2)
+
+	return HttpResponse ("El resultado es " + str(rest)
+
+def multiplicacion (request, num1, num2):
+
+	mult =  int(num1) * int(num2)
+
+	return HttpResponse ("El resultado es " + str(mult)
+
+def division (request, num1, num2):
+
+	div =  int(num1) / int(num2)
+
+	return HttpResponse ("El resultado es " + str(div)
